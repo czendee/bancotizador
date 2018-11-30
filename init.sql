a43ce7444ed9aa1d8a58bb8964fa6cb17d476b49
tochi <czendee@gmail.com>
2018-11-30 15:40:07 -0600

    first commit banwire gs 3 services

---
+++ init.sql
@@ -0,0 +1,28 @@
+CREATE USER banwire;
+
+CREATE DATABASE gs_ivr_tokenization OWNER banwire;
+
+
+CREATE TABLE banwirecard(
+id_card bigserial,
+token    varchar(100),   --constrain unique
+last_digits   varchar(4),        
+bin         varchar(6),
+valid_thru    timestamp, 
+score     integer,
+is_banned          boolean,
+banned_at         timestamp, 
+created_at         timestamp,
+last_update_at timestamp ,
+id_customer      bigint,
+brand   varchar(20) ,
+type_card varchar(15)
+);
+
+
+CREATE TABLE banwirecustomer(
+id_customer bigserial,
+reference    varchar(100),   --constrain unique
+created_at         timestamp,
+last_update_at timestamp 
+)
