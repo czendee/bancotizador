f8cfe771a867af67db4a3b4fde0c2c10d4316d1f
813743 <carlosisrael.zendejas@fedex.com>
2019-04-08 16:17:02 -0500

    3.9 fix amount with comma cr.banwire

--- gs_ivr_tokenization/logicweb.go
+++ gs_ivr_tokenization/logicweb.go
@@ -36,10 +36,10 @@ import (
 		"user": {Config_WS_crbanwire_pass},  //this value needs to be configurable to Move to production. 22 Jan 2019        
 		"reference": {requestData.Paymentreference}, 
 		"token": {requestData.Token},         
-		"amount": {strings.Trim(requestData.Amount, ",")},          
+		"amount": {strings.Replace(requestData.Amount, ",", "", -1)  },          
         "cvv": {requestData.Cvv},  
 	}
-log.Println("web api el amount"+strings.Trim(requestData.Amount, ","))    
+log.Println("web api el amount 6:"+strings.Replace(requestData.Amount, ",", "", -1)  )    
 log.Println("web api el cvv"+requestData.Cvv)
 //	    response,err := http.PostForm("https://cr.banwire.com/?action=card",
 	    response,err := http.PostForm(Config_WS_crbanwire_url+"/?action=card",
